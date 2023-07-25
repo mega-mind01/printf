e3a756b89c7dcde39920e9baee126c5ba80a6d79
mega-mind01 <muyiwameleki@gmail.com>
2023-07-25 14:31:51 -0700

    handle more cases

---
+++ utils.c
@@ -0,0 +1,93 @@
+#include "main.h"
+
+/**
+ * is_printable - Evaluates printable char
+ * @c: Char
+ *
+ * Return: 1 if printable, 0 otherwise
+ */
+
+int is_printable(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+
+	return (0);
+}
+
+/**
+ * append_hexa_code - Append ascci to buffer
+ * @buffer: Array
+ * @i: Index to start appending.
+ * @ascii_code: ASSCI CODE.
+ * Return: Always 3
+ */
+
+int append_hexa_code(char ascii_code, char buffer[], int i)
+{
+	char map_to[] = "0123456789ABCDEF";
+
+	if (ascii_code < 0)
+		ascii_code *= -1;
+
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+
+	return (3);
+}
+
+/**
+ * is_digit - checks if a char is a digit
+ * @c: Char
+ *
+ * Return: 1 if digit, 0 otherwise
+ */
+
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
+
+/**
+ * convert_size_number - Casts a number
+ * @num: Number casted.
+ * @size: Number indicator
+ *
+ * Return: Casted value
+ */
+
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+
+	return ((int)num);
+}
+
+/**
+ * convert_size_unsgnd - Casts a number
+ *
+ * @num: Number casted
+ * @size: Number indicator
+ *
+ * Return: Casted value
+ *
+ */
+
+long int convert_size_unsgnd(unsigned long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
+}
